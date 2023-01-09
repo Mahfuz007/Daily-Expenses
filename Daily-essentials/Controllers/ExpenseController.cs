cabcecf58c1c29d94650ef814feba6edd9fdf9c4
Mahfuz <aamahfuz2@gmail.com>
2023-01-09 22:53:11 +0600

    change structure

---
+++ Daily-essentials/Controllers/ExpenseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Daily_essentials.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+    }
+}
