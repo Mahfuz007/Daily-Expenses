b2ca7c9c8bcfcf59ce2507730e53c9b89485dd4d
Mahfuz <aamahfuz2@gmail.com>
2023-01-10 23:28:28 +0600

    category entity added

---
+++ Application/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Entities
+{
+    public class Category: ExpenseEntityBase
+    {
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsExpense { get; set; } = false;
+    }
+}
---
+++ Application/Entities/ExpenseEntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Entities
+{
+    public class ExpenseEntityBase
+    {
+        public string ItemId { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? UpdatedBy { get; set; }   
+        public DateTime UpdatedDate { get; set; }
+    }
+}
